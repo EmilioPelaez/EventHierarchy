09c05b16e2de7236e92c746ea18fe6b236727f66
Emilio Peláez <me@epelaez.net>
2021-12-30 21:48:31 +0100

    Example Project

---
+++ Example/ActionHierarchyExample/ActionHierarchyExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActionHierarchyExampleApp.swift
+//  ActionHierarchyExample
+//
+//  Created by Emilio Peláez on 30/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ActionHierarchyExampleApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
